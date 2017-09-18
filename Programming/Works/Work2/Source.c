cfa264c87f285ed9780d5915d2357697e8a1ec2d
Alexey Novopashin <allordiron@gmail.com>
2017-09-18 18:02:37 +0300

    Modify gitignore file, complete work 2 programming lab

---
+++ Programming/Works/Work2/Source.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+const float PI = 3.14159;
+
+float degree_to_radian(float degree)
+{
+	return degree * PI / 180;
+}
+
+float function_first(float radian)
+{
+	return 2 * pow(sin(3 * PI - 2 * radian), 2) * pow(cos(5 * PI + 2 * radian), 2);
+}
+
+float function_second(float radian)
+{
+	return 0.25 - 0.25 * sin(2.5 * PI - 8 * radian);
+}
+
+void pause()
+{
+	printf("\nPlease enter any key to exit... \n");
+	getchar();
+	getchar();
+}
+
+// Вариант 1
+int main()
+{
+	// Пользователь вводит радиану или градусную меру угла?
+	float degree;
+	printf("Enter degree: ");
+	scanf_s("%f", &degree);
+
+	const float radian = degree_to_radian(degree);
+	printf("Z1: %f\n", function_first(radian));
+	printf("Z2: %f\n", function_second(radian));
+
+	pause();
+	return 0;
+}
