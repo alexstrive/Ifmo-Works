1a229a21784daabb58785714fabeb0bf296c5e0f
Alexey Novopashin <allordiron@gmail.com>
2017-10-01 13:47:59 +0300

    Rewrite DB Work 3 Query 6

--- Databases/Work 3/4-4.sql
+++ Databases/Work 3/4-4.sql
@@ -1,10 +1,10 @@
 select FirstName, LastName, Name
 from Purchasing.VendorContact
 
-	/* get job title of agents */
+	/* get job title of agent */
 	inner join Person.ContactType
 	on VendorContact.ContactTypeID = ContactType.ContactTypeID
 
-	/* get first names and last names of agents */
+	/* get first name and last name of agent */
 	inner join Person.Contact
 	on VendorContact.ContactID = Contact.ContactID
--- Databases/Work 3/4-6.sql
+++ Databases/Work 3/4-6.sql
@@ -1,7 +1,13 @@
 select Product.Name, Vendor.Name
 from Production.Product
+
+	/* get vendor of product */
 	left join Purchasing.ProductVendor
-	on ProductVendor.ProductID = Product.ProductID
+	on Product.ProductID = ProductVendor.ProductID
+
+	/* get name of vendor */
 	left join Purchasing.Vendor
 	on ProductVendor.VendorID = Vendor.VendorID
+
+/* search for tourist product*/
 where ProductLine = 'T'
