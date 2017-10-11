aef038ba99e6f6fae88934799d12b946aca62617
Alexey Novopashin <allordiron@gmail.com>
2017-10-11 14:22:01 +0300

    Complete Programming Work 8. Delete old works.

--- Programming/Works/work8.c
+++ Programming/Works/work8.c
@@ -5,39 +5,90 @@ void showTitle(const int task_number) {
     printf("--- Task %d ---\n", task_number);
 }
 
-void compareTwoStrings(const char *firstString, const char *lastString) {
-    int resultComparing = strcmp(firstString, lastString);
+/**
+ * Compare two strings
+ */
+void compareStrings(const char *firstString, const char *secondString) {
+    int comparingResult = strcmp(firstString, secondString);
 
-    if (resultComparing == 0) {
-        printf("[%s] equals [%s]\n", firstString, lastString);
+    if (comparingResult == 0) {
+        printf("[%s] equals [%s]\n", firstString, secondString);
     }
-    else if (resultComparing < 0) {
-        printf("[%s] precedes [%s]\n", firstString, lastString);
+    else if (comparingResult < 0) {
+        printf("[%s] precedes [%s]\n", firstString, secondString);
     }
-    else if (resultComparing > 0) {
-        printf("[%s] follows [%s]\n", firstString, lastString);
+    else if (comparingResult > 0) {
+        printf("[%s] follows [%s]\n", firstString, secondString);
     }
 }
 
-void compareSymbolsTwoStrings(const char *firstString, const char *lastString, int numberSymbols) {
-    int resultComparing = strncmp(firstString, lastString, numberSymbols)
+/**
+ * Compare symbols of first and second strings.
+ * Prefer to use strncmp(firstString, secondString, amount)
+ */
+void compareSymbolsStrings(const char *firstString, const char *secondString, unsigned amountSymbols) {
+    char firstPart[256];
+    char secondPart[256];
+
+    strncpy(firstPart, firstString, amountSymbols);
+    strncpy(secondPart, secondString, amountSymbols);
+
+    compareStrings(firstPart, secondPart);
+}
+
+/**
+ * Split strings into tokens and print them
+ */
+void splitString(char *string, char *separators) {
+    printf("Splitting up the input string [%s]\n", string);
+
+    char *token = strtok(string, separators);
+    while (token) {
+        puts(token);
+        token = strtok(NULL, separators);
+    }
+}
+
+/**
+ * Get position of symbols from set
+ */
+int getPosition(const char *phrase, const char *symbolsSet) {
+    return (int) (strpbrk(phrase, symbolsSet) - phrase);
 }
 
 // Variant 17
 int main() {
-    char *phrase = "Hello world";
+    char phrase[] = "Hello world";
     char *firstPart = "Hello";
-    char *secondPart = "world";
 
     // Task 1 (3)
-    compareTwoStrings(phrase, firstPart);
+    showTitle(1);
+    compareStrings(phrase, firstPart);
 
     // Task 2 (4)
-    compareSymbolsTwoStrings(phrase, firstPart, 4);
+    showTitle(2);
+    compareSymbolsStrings(phrase, firstPart, 5);
 
     // Task 3 (7)
+    showTitle(3);
+    int phraseLength = (int) strlen(phrase);
+    printf("Length of phrase is %d symbols\n", phraseLength);
+
     // Task 4 (10)
+    showTitle(4);
+    char *targetSymbols = " od";
+
+    printf("Symbol from set has found at [%d] position\n", getPosition(phrase, targetSymbols));
+
     // Task 5 (13)
+    showTitle(5);
+
+    char loremIpsum[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            " Duis semper odio at lacinia volutpat."
+            " Suspendisse potenti."
+            " Mauris vehicula velit volutpat malesuada vestibulum.";
+
+    splitString(loremIpsum, " ");
 
     return 0;
 }
