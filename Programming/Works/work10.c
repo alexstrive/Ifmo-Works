474b77bae45d5ac8a9cc8d06aae20a8b2cf626c0
Alexey Novopashin <allordiron@gmail.com>
2017-10-16 21:12:22 +0300

    Complete programming work 10

---
+++ Programming/Works/work10.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include "utils.h"
+
+int sumDigits(int number) {
+    if (number < 10) {
+        return number;
+    }
+    else {
+        return number % 10 + sumDigits(number / 10);
+    }
+}
+
+int gcd(int a, int b) {
+    int c;
+
+    while (a) {
+        c = a;
+        a = b % a;
+        b = c;
+    }
+
+    return b;
+}
+
+int lcm(int a, int b) {
+    return a * (b / gcd(a, b));
+}
+
+// Variant 19
+int main() {
+    // Task 1
+    showTaskTitle();
+    // e.g. 45 and 78 – GCD 3 LCM 1170
+    int firstNumber, secondNumber;
+
+    printf("Enter first number value: ");
+    scanf("%d", &firstNumber);
+
+    printf("Enter second number value: ");
+    scanf("%d", &secondNumber);
+
+    printf("\nGCD: %d", gcd(firstNumber, secondNumber));
+    printf("\nLCM: %d\n\n", lcm(firstNumber, secondNumber));
+
+    // Task 2
+    showTaskTitle();
+
+    int number;
+    printf("Enter a number value: ");
+    scanf("%d", &number);
+
+    printf("\nSum of digits: %d", sumDigits(number));
+
+    return 0;
+}
