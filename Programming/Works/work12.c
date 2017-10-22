5f5e38684658b9184e00450f03413811112747f4
Alexey Novopashin <allordiron@gmail.com>
2017-10-22 22:44:31 +0300

    Complete programming work 12

---
+++ Programming/Works/work12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int addNumbers(int a, int b) {
+    return a + b;
+}
+
+int main(int argCount, char *argv[]) {
+    freopen("../output.txt", "w", stdout);
+
+    int firstNumber = atoi(argv[1]);
+    int secondNumber = atoi(argv[2]);
+    int sumNumber = addNumbers(firstNumber, secondNumber);
+
+    printf("%d + %d = %d", firstNumber, secondNumber, sumNumber);
+
+    return 0;
+}
+
